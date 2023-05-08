8eff66ca7c90dad10424fd10ff05a37e510af6e9
pentran <penny.tran@stonybrook.edu>
2023-05-08 06:22:09 +0000

    fixed main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ df = pd.read_csv('./data/smallservices2019.csv')
 
 app = FastAPI()
 
-@app.route('/', methods=["GET"])
-async def home():
+@app.get('/')
+async def root():
     return {'this is a API service for MN SVC code details'}
 
-@app.route('/preview', methods=["GET"])
+@app.get('/preview')
 async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return {result}
 
-@app.route('/svc/<value>', methods=['GET'])
+@app.get('/svc/<value>')
 async def svccode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
@@ -24,7 +24,7 @@ async def svccode(value):
     else: 
         return {filtered.to_json(orient="records")}
 
-@app.route('/svc/<value>/sex/<value2>')
+@app.get('/svc/<value>/sex/<value2>')
 async def svccode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
