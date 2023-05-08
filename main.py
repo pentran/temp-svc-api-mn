29ac15d9f2e7d9a27bb24486eb183089b170174f
pentran <penny.tran@stonybrook.edu>
2023-05-08 04:57:07 +0000

    added main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI 
+import pandas as pd 
+
+df = pd.read_csv('./data/smallservices2019.csv')
+
+app = FastAPI()
+
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is a API service for MN SVC code details'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/svc/<value>', methods=['GET'])
+def svccode(value):
+    print('value: ', value)
+    filtered = df[df['principal_diagnosis_code'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.route('/svc/<value>/sex/<value2>')
+def svccode2(value, value2):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
